fd1687398e22b9f486e0852cd6a9832885f5f4a7
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-3.local>
2022-03-22 12:47:25 +0100

    WP

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>Hola</div>
+  <!-- 3. utilitzar el component al template -->
+  <!-- <HelloWorld />
+  <HelloWorld /> -->
+  <Counter/>
+</template>
+<script>
+// 1: importar el component
+import HelloWorld from "./components/HelloWorld.vue";
+import counter from "./components/Counter.vue";
+export default {
+  // 2: registrar el component
+  components: {
+    HelloWorld,
+    Counter
+  },
+};
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-100 mb-4">
+    <div>The count is: {{ count }}</div>
+    <button class="border px-4 py-2 rounded" @click="count++">+1</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
